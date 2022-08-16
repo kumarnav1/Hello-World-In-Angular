b025842bedcb73e6fcb568ffc4b6964ace3f5068
nk400 <navkr007@gmail.com>
2022-08-16 23:26:30 +0530

    ADDED: Imported Forms Module and used two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "../assets/Bl_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com";
+  userName: string = "";
 
  ngOnInit(): void {
   this.title = "Hello from Bridgelabz.";
@@ -19,8 +20,6 @@ export class AppComponent {
   console.log("Save button is clicked!", $event);
   window.open(this.url, "_blank");
 }
-
-
 
 
 }
