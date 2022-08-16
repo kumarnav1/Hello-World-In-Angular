419bbc7f66124fe101184b06d283bdfb7d4c1a76
nk400 <navkr007@gmail.com>
2022-08-16 22:59:57 +0530

    ADDED: Event binding using mouse click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,18 @@ export class AppComponent {
   
   title = 'HelloWorld';
   imgUrl = "../assets/Bl_logo_square_jpg.jpg";
+  url = "https://www.bridgelabz.com";
 
  ngOnInit(): void {
   this.title = "Hello from Bridgelabz.";
  } 
+
+ onClick($event: any){
+  console.log("Save button is clicked!", $event);
+  window.open(this.url, "_blank");
+}
+
+
+
+
 }
