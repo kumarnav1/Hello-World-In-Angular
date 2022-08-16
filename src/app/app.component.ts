95d1ea1187f7632efacd7e42dfa81f63c8f80c2f
nk400 <navkr007@gmail.com>
2022-08-17 00:17:24 +0530

    ADDED: Validation to the input added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   imgUrl = "../assets/Bl_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com";
   userName: string = "";
+  nameError: string= "";
 
  ngOnInit(): void {
   this.title = "Hello from Bridgelabz.";
@@ -21,5 +22,13 @@ export class AppComponent {
   window.open(this.url, "_blank");
 }
 
-
+onInput($event:any){
+  console.log("change Event Occured", $event.data);
+  const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+  if(nameRegex.test(this.userName)){
+    this.nameError="";
+    return;
+  }
+  this.nameError="Name is Incorrect";
+}
 }
